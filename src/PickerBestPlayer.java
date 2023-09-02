8c04d5bd4ca3fea476324140d7a0b64803e17eea
bax <tmdwls8387@ajou.ac.kr>
2023-09-03 00:37:10 +0900

    PickerBestPlayer 업로드

---
+++ src/PickerBestPlayer.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class PickerBestPlayer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<String> playerNumbers = new ArrayList<String>();
+        List<Double> playerRecords = new ArrayList<Double>();
+
+
+        while (true) {
+
+            System.out.println("선수의 번호를 입력하세요.");
+            playerNumbers.add(sc.nextLine());
+
+            if (playerNumbers.contains("print")) {   //.contains : 값이 존재하는지 확인하는 메서드
+                double max = findMaxValue((ArrayList<Double>) playerRecords);
+                //System.out.println(max);
+
+                for (int i = 0; i < playerRecords.size(); i++) {
+                    if (playerRecords.get(i).equals(max)) {
+
+                        System.out.println("1등 : "+playerNumbers.get(i)+"번 선수 / "+ max+"초 (참가인원 : "+playerRecords.size()+"명)");
+                    }
+                }
+                break;
+            }
+
+            System.out.println("이 선수의 100m 달리기 기록이 몇 초인지 입력하세요.");
+            double bf_round = sc.nextDouble();
+            playerRecords.add(Math.round(bf_round * 100.0) / 100.0); // 입력값을 소수 둘째 자리까지 반올림하여 저장
+            sc.nextLine(); //scan.nextLine() 을 한번 더 작성하여 개행문자 제거
+
+
+
+        }
+        sc.close();
+    }
+
+
+
+    public static double findMaxValue(ArrayList<Double> arrayList) { //가장 큰 수 찾는 메서드
+
+
+        double max = arrayList.get(0);
+
+
+        for (int i = 1; i < arrayList.size(); i++) {
+            double current = arrayList.get(i);
+            if (current > max) {
+                max = current;
+            }
+        }
+
+        return max;
+    }
+}
+
